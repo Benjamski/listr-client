c8f1aa475cf8dbd2fe88344231485f367090953d
Jeffrey Horn <jeffh@generalassemb.ly>
2016-03-16 15:46:50 -0400

    Associate items with list

--- app/item/model.js
+++ app/item/model.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  done: DS.attr('boolean')
   content: DS.attr('string'),
+  done: DS.attr('boolean'),
+  list: DS.belongsTo('list'),
 });
